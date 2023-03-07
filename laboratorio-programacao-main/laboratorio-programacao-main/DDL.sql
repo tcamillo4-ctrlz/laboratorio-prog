0c28d7034e2de6b447a8508de0902981797a07f3
Tiago Camillo <66873418+tcamillo4-ctrlz@users.noreply.github.com>
2023-03-07 19:00:03 -0300

    update proj

---
+++ laboratorio-programacao-main/laboratorio-programacao-main/DDL.sql
@@ -0,0 +1,15 @@
+create schema anotacao;
+
+use anotacao;
+
+create user 'user'@'localhost' identified by 'pass123';
+
+grant select, insert, delete, update on anotacao.* to user@'localhost';
+
+create table usr_usuario (
+  usr_id bigint unsigned not null auto_increment,
+  usr_nome varchar(20) not null,
+  usr_senha varchar(150) not null,
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
